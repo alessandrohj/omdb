5325d3e6f16e164172ae440a398f0e711c9d785f
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-05-05 14:21:06 -0400

    First commit. Initial structure

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const APP = {
+    base_URL = 'http://www.omdbapi.com/?apikey=',
+    API_KEY = '898dd5ab',
+    init () {
+
+    },
+    listeners () {
+
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', APP.init);
